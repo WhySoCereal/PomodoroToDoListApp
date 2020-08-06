2d554ccf257626f759a763a2cc46cc034f01b1d3
Brian Alldred <bri_alldred@live.co.uk>
2020-08-06 12:42:55 +0100

    Initial Commit

---
+++ PomodoroToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PomodoroToDo
+//
+//  Created by Brian Alldred on 06/08/2020.
+//  Copyright © 2020 Brian Alldred. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
